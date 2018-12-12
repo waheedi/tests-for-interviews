9c867dd6590c41ad833dd41868fe7db113e4b2b1
Waheed Barghouthi <waheed.barghouthi@gmail.com>
2018-12-12 10:46:12 +0100

    indentation

--- intervals_test.rb
+++ intervals_test.rb
@@ -14,26 +14,26 @@ def interval_find(arr) #arr => [ {0=>1}, {2=>4}, {6=>7}, {3=>5} ]
   arr = arr.sort_by{|hsh| hsh.keys[0]}
   delete = []
   flag = false
-	arr.each_with_index { |a,i|
-		 if i+1 < arr.length && a.values[0] >= arr[i+1].keys[0] 
-		 	 result << {a.keys[0] => arr[i+1].values[0]}
-		 	 delete << i+1
-		 	 flag = true 
-		 else
-		   result << {a.keys[0] => a.values[0]}
-		 end
-	}
   
-	delete.each { |i|
-		result.delete_at(i)
-		arr.delete_at(i)
-	}
+  arr.each_with_index { |a,i|
+    if i+1 < arr.length && a.values[0] >= arr[i+1].keys[0] 
+      result << {a.keys[0] => arr[i+1].values[0]}
+      delete << i+1
+      flag = true 
+    else
+      result << {a.keys[0] => a.values[0]}
+    end
+  }
 
-	if flag
-		interval_find(result)
-	else
-		return result
-	end
+  delete.each { |i|
+    result.delete_at(i)
+  }
+
+  if flag
+    interval_find(result)
+  else
+    return result
+  end
   
 end
 
