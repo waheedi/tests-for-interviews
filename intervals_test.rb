2bc77b5c6d8779bcc67a7cad825ceac5e09dd720
Waheed Barghouthi <waheed.barghouthi@gmail.com>
2018-12-13 11:32:22 +0100

    Update intervals_test.rb

--- intervals_test.rb
+++ intervals_test.rb
@@ -1,15 +1,4 @@
-#Merging overlapping intervals
-#You need to write a function that reads a list of intervals from STDIN , 
-#merges the intervals that are overlapping and write the resulting list to STDOUT
-#If your function reads a list of intervals that contain (2,4) and (3,5), 
-#because those two intervals have an overlap they should be merged into a single
-#interval (2,5)
-#Sample Input
-#[0,1][2,4][6,7][3,5]
-#Sample Output
-#[0,1][2,5][6,7]
-
-def interval_find(arr) #arr => [ {0=>1}, {2=>4}, {6=>7}, {3=>5} ]
+def interval_find_overlap(arr) #arr => [ {0=>1}, {2=>4}, {6=>7}, {3=>5} ]
   result = []
   arr = arr.sort_by{|hsh| hsh.keys[0]}
   delete = []
@@ -34,6 +23,9 @@ def interval_find(arr) #arr => [ {0=>1}, {2=>4}, {6=>7}, {3=>5} ]
   else
     return result
   end
-  
 end
+
+#usage notes
+#interval_find_overlap([ {0=>1}, {2=>4}, {6=>7}, {3=>5} ])
+
 
